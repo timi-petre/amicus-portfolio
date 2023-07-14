800bd81e18e48439cdf93d3e41e5b0477557d9c5
timi-petre <12413810+timi-petre@users.noreply.github.com>
2023-07-14 18:54:10 +0300

    Tic tac toe on `projects`

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import * as photos from './proiecte.json';
 
-export default function Projects() {
+async function getData() {
+	// const res = await fetch('./proiecte.json');
+	let datas = JSON.stringify(photos);
+
+	// if (!res.ok) {
+	// 	throw new Error('Failed to fetch data');
+	// }
+
+	return datas;
+}
+
+export default async function Projects() {
+	const data = await getData();
 	return (
 		<>
 			<div className="flex flex-col justify-center text-center">
@@ -18,15 +31,13 @@ export default function Projects() {
 								<Image
 									src="/../public/imgs/tic-tac-toe.png"
 									// fill
-									alt="Tic-tac-toe"
+									alt={data.image.alt}
 									width={200}
 									height={279}
 									quality={100}
 								/>
 							</Link>
 						</div>
-						<div className="text-gray-700 text-center  px-4 py-2 m-2">2</div>
-						<div className="text-gray-700 text-center  px-4 py-2 m-2">3</div>
 					</div>
 					<button className="bg-transparent hover:bg-slate-100 text-white-700 font-semibold hover:text-black py-2 px-4 border border-white-500 hover:border-transparent rounded">
 						View more
