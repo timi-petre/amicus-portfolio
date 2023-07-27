6bca0ecb74cbd8e982c259df8c4724a36be86522
timi-petre <12413810+timi-petre@users.noreply.github.com>
2023-07-27 17:51:13 +0300

    text fonts modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import style from './styles/components/Home.module.css';
 
 export default function Home() {
-	let shortDescription = `I Am Passionate Developer`;
+	let shortDescription = `I Am Passionate `;
 	let longDescription =
 		'The namics of how users interact with interactive elements within a user interface flow chart based on container proportion.';
 	let intro = 'Hello there...';
@@ -10,22 +10,30 @@ export default function Home() {
 	return (
 		<>
 			<div className="flex justify-around">
-				<div className="flex flex-col justify-center">
-					<p>{intro}</p>
-					<h1>{fullName}</h1>
-					<p>{shortDescription}</p>
-					<p>{longDescription}</p>
+				<div className="flex flex-col justify-center gap-3">
+					<p className="text-xl font-semibold text-gray-900 dark:text-white">{intro}</p>
+					<h1 className="text-6xl font-semibold text-gray-900 dark:text-white">{fullName}</h1>
+					<p className="text-xl font-semibold text-gray-900 dark:text-white">
+						{shortDescription}
+						<br className="block md:hidden" />
+						<span
+							className={`inline-flex h-10 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent text-2xl ${style.will_change_transform}`}
+						>
+							Developer
+						</span>
+					</p>
+					<p className=" text-gray-300 dark:text-gray-400">{longDescription}</p>
 					<br />
 					<div className="flex">
 						<button
 							type="button"
-							className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ease-in duration-300"
+							className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ease-in duration-200"
 						>
 							My Work
 						</button>
 						<button
 							type="button"
-							className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-black dark:hover:bg-gray-100 dark:focus:ring-gray-800 ease-in duration-300"
+							className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-black dark:hover:bg-gray-100 dark:focus:ring-gray-800 ease-in duration-200"
 						>
 							Hire Me
 						</button>
