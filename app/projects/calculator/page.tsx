cc7e4b2a97d9a698bc7b4149094273cb34713ba4
timi-petre <12413810+timi-petre@users.noreply.github.com>
2023-08-03 15:50:49 +0300

    Added method for showInput

--- app/projects/calculator/page.tsx
+++ app/projects/calculator/page.tsx
@@ -3,9 +3,11 @@
 import { useState } from 'react'
 import styles from './styles/Calculator.module.css'
 
-function ButtonSquare({ value }: { value: string }) {
+function ButtonSquare({ value, onClick }: { value: any; onClick: (value: any) => void }) {
 	function handleClick(e: any) {
-		console.log('clicked ' + e.target.value)
+		e.preventDefault()
+		console.log('clicked ' + value)
+		onClick(value)
 	}
 
 	return (
@@ -18,45 +20,19 @@ function ButtonSquare({ value }: { value: string }) {
 export default function Board() {
 	const [val, setVal] = useState('0')
 
-	function handleChange(e: any) {
-		setVal(e.target.value)
-	}
-
-	function handleClick(e: any) {
-		alert('clicked ' + e.target.value)
-		ButtonSquare(e.target.value)
-	}
-
-	function handleClear() {
-		setVal('0')
-	}
-
-	function handleDel() {
-		if (val.length > 1) {
-			setVal(val.slice(0, -1))
-		} else {
+	function showInput(value: any) {
+		if (value === 'AC') {
 			setVal('0')
-		}
-	}
-
-	function handleEqual() {
-		let result = eval(val)
-		setVal(result.toString())
-	}
-
-	function handleDot() {
-		if (val.indexOf('.') === -1) {
-			setVal(val + '.')
-		}
-	}
-
-	function handleAC() {
-		setVal('0')
-	}
-
-	function handleNumber(e: any) {
-		if (val === '0') {
-			setVal(e.target.value)
+		} else if (value === 'DEL') {
+			setVal(prevVal => prevVal.slice(0, -1) || '0')
+		} else if (value === '=') {
+			try {
+				setVal(eval(val).toString())
+			} catch (error) {
+				setVal('Error')
+			}
+		} else {
+			setVal(prevVal => (prevVal === '0' ? value : prevVal + value))
 		}
 	}
 
@@ -65,50 +41,76 @@ export default function Board() {
 			<h1 className="text-center my-5 text-6xl">Calculator</h1>
 			<div className="text-center flex justify-center align-middle my-20">
 				<div id={styles.calculator}>
-					<input
-						type="text"
-						name=""
-						id={styles.display}
-						value={val}
-						onChange={handleChange}
-						disabled
-					/>
+					<input type="text" name="" id={styles.display} value={val} disabled />
 					<br />
 					<div className="board-row">
-						<ButtonSquare value="AC" />
-						<ButtonSquare value="DEL" />
-						<ButtonSquare value="." />
-						<ButtonSquare value="/" />
+						<ButtonSquare value="AC" onClick={showInput} />
+						<ButtonSquare value="DEL" onClick={showInput} />
+						<ButtonSquare value="." onClick={showInput} />
+						<ButtonSquare value="/" onClick={showInput} />
 					</div>
 					<br />
 					<div className="board-row">
-						<ButtonSquare value="7" />
-						<ButtonSquare value="8" />
-						<ButtonSquare value="9" />
-						<ButtonSquare value="*" />
+						<ButtonSquare value="7" onClick={showInput} />
+						<ButtonSquare value="8" onClick={showInput} />
+						<ButtonSquare value="9" onClick={showInput} />
+						<ButtonSquare value="*" onClick={showInput} />
 					</div>
 					<br />
 					<div className="board-row">
-						<ButtonSquare value="4" />
-						<ButtonSquare value="5" />
-						<ButtonSquare value="6" />
-						<ButtonSquare value="-" />
+						<ButtonSquare value="4" onClick={showInput} />
+						<ButtonSquare value="5" onClick={showInput} />
+						<ButtonSquare value="6" onClick={showInput} />
+						<ButtonSquare value="-" onClick={showInput} />
 					</div>
 					<br />
 					<div className="board-row">
-						<ButtonSquare value="1" />
-						<ButtonSquare value="2" />
-						<ButtonSquare value="3" />
-						<ButtonSquare value="+" />
+						<ButtonSquare value="1" onClick={showInput} />
+						<ButtonSquare value="2" onClick={showInput} />
+						<ButtonSquare value="3" onClick={showInput} />
+						<ButtonSquare value="+" onClick={showInput} />
 					</div>
 					<br />
 					<div className="board-row">
-						<ButtonSquare value="00" />
-						<ButtonSquare value="0" />
-						<ButtonSquare value="=" />
+						<ButtonSquare value="00" onClick={showInput} />
+						<ButtonSquare value="0" onClick={showInput} />
+						<ButtonSquare value="=" onClick={showInput} />
 					</div>
 				</div>
 			</div>
 		</>
 	)
 }
+
+// function handleClear() {
+// 	setVal('0')
+// }
+
+// function handleDel() {
+// 	if (val.length > 1) {
+// 		setVal(val.slice(0, -1))
+// 	} else {
+// 		setVal('0')
+// 	}
+// }
+
+// function handleEqual() {
+// 	let result = eval(val)
+// 	setVal(result.toString())
+// }
+
+// function handleDot() {
+// 	if (val.indexOf('.') === -1) {
+// 		setVal(val + '.')
+// 	}
+// }
+
+// function handleAC() {
+// 	setVal('0')
+// }
+
+// function handleNumber(e: any) {
+// 	if (val === '0') {
+// 		setVal(e.target.value)
+// 	}
+// }
